793674d385449a21298bab87b27efef637f6f0c3
ViniMar10 <viniciuscauamartins@hotmail.com>
2025-07-30 17:21:15 -0300

    Setup url repository
    Signed-off-by: ViniMar10 <viniciuscauamartins@hotmail.com>

---
+++ url-shortner-backend/src/main/java/com/vini/url_shortner_backend/repository/UrlRepository.java
@@ -0,0 +1,11 @@
+package com.vini.url_shortner_backend.repository;
+
+import com.vini.url_shortner_backend.entity.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UrlRepository extends JpaRepository<Url, Long> {
+    Optional<Url> findByShortCode(String shortCode);
+    boolean existsByShortCode(String shortCode);
+}
